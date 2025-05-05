998677d1f4684e8b560c0bcfa3463061c12aa497
RodrigoRamos2076223 <2076223@student.uma.pt>
2025-05-05 11:38:51 +0100

    ficha10

--- ficha10/ficha10/routes/books.js
+++ ficha10/ficha10/routes/books.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var booksController = require('../controllers/booksController');
 
-router.get('/', booksController.getAllBook);
+router.get('/', booksController.getAllBooks);
+router.get('/:id', booksController.getBookById);
+router.post('/', booksController.createBook);
+router.delete('/', booksController.deleteBook);
+router.put('/', booksController.updateBook);
 
-  module.exports = router;
+module.exports = router;
